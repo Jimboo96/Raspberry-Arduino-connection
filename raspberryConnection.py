63d7cbc6749a06e61bf1c4db79735e9796d19314
Jimboo96 <32327296+Jimboo96@users.noreply.github.com>
2017-12-12 11:08:07 +0200

    Add files via upload

---
+++ raspberryConnection.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+import MySQLdb, serial, sys, datetime, time
+db = MySQLdb.connect("localhost","Humppa", "salaovi","Taika_akkuna" )
+
+def sqlPush(temperature, humidity, timeAndDate):
+
+      cursor = db.cursor()
+
+      sql = "INSERT INTO DATA (Lampo, Aika_paiva, Kosteus) \
+             VALUES ('%s','%s', '%s')" % \
+             (temperature, timeAndDate, humidity)
+      try:
+         cursor.execute(sql)
+         db.commit()
+         print(timeAndDate)
+      except:
+         print("Error: unable to push data")
+         db.rollback()
+
+      return
+
+with serial.Serial(port='/dev/ttyACM0', baudrate=9600) as ser:
+
+    if ser.isOpen():
+
+      ser.readline()
+
+    while ser.isOpen():
+
+      serialData = (ser.readline()).decode("utf-8")
+      serialData = serialData[:-2]
+      timeAndDate = datetime.datetime.now()
+      timeAndDate = str(timeAndDate)
+      timeAndDate = timeAndDate[:-7]
+      temperatureDetector = "C" in serialData
+      humidityDetector = "H" in serialData
+
+      if(temperatureDetector == 1):
+        serialData = serialData[:-1]
+        temperature = serialData
+
+      if(humidityDetector == 1):
+        serialData = serialData[:-1]
+        humidity = serialData
+        sqlPush(temperature, humidity, timeAndDate)
